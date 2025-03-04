8456fd340b39e14c0f79fce2ff8c944dbffafd29
신동훈 <huipulci1@naver.com>
2025-03-04 13:11:12 +0900

    [#1] feat: JWT 기능 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,62 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "mallang"
 version = "1.0.0"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
-	implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-	implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
-	// JWT
-	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
-	// MapStruct
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    // MapStruct
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
 
-	compileOnly("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
 
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-	annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	// Fixture Monkey
-	testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.1.9")
+    // Fixture Monkey
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.1.9")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
---
+++ src/test/java/cupid/auth/TokenServiceTest.java
@@ -0,0 +1,48 @@
+package cupid.auth;
+
+import static cupid.auth.TokenExceptionCode.EXPIRED_TOKEN;
+import static cupid.auth.TokenExceptionCode.INVALID_TOKEN;
+
+import cupid.support.UnitTest;
+import org.junit.jupiter.api.Test;
+
+class TokenServiceTest extends UnitTest {
+
+    private TokenService tokenService = new TokenService(new TokenProperty("sdadsasad".repeat(10), 100000000));
+
+    @Test
+    void 만료된_토큰_사용_시_예외() {
+        // given
+        TokenService tokenService = new TokenService(new TokenProperty("sdadsasad".repeat(10), 1));
+
+        // when
+        Token token = tokenService.createToken(1L);
+
+        // then
+        exceptionTest(() -> {
+            tokenService.extractMemberId(token.accessToken());
+        }, EXPIRED_TOKEN);
+    }
+
+    @Test
+    void 토큰_형식이_올바르지_않으면_예외() {
+        // given
+        Token token = new Token("dsadsa");
+
+        // when & then
+        exceptionTest(() -> {
+            tokenService.extractMemberId(token.accessToken());
+        }, INVALID_TOKEN);
+    }
+
+    @Test
+    void 토큰_내용이_올바르지_않은_경우_예외() {
+        // given
+        Token token = new Token("dsadsa.b.c");
+
+        // when & then
+        exceptionTest(() -> {
+            tokenService.extractMemberId(token.accessToken());
+        }, INVALID_TOKEN);
+    }
+}
